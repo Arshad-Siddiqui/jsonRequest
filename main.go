15a6552cb4b3ff58bc87f31b40ca63791479ac7e
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-05-17 13:52:42 +0100

    Write #postJsonReq

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package jsonRequest
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
 
-func main() {
-	fmt.Println("Project Setup")
+func postJsonReq(requestRoute string, requestObject interface{}) (*http.Request, error) {
+	req, err := http.NewRequest("POST", requestRoute, nil) // "/"
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	json, err := json.Marshal(requestObject)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Body = io.NopCloser(bytes.NewBuffer(json))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	return req, nil
 }
